f027077fbcaa8761e3260b9a02ae17a6aead1d92
chrystalquek <chrystalquek@yahoo.com.sg>
2025-08-24 23:06:54 +0800

    Add matcha experiences

--- assets/js/matcha.js
+++ assets/js/matcha.js
@@ -53,7 +53,7 @@ const matchaExperiences = [
     },
     {
         location: 'KOKO Cafe and Patisserie',
-        dateTime: new Date(2024, 3, 26),
+        dateTime: new Date(2025, 3, 26),
         cardImages: ['assets/images/matcha/koko.HEIC'],
         gmapsLink: 'https://g.co/kgs/C2V61kP',
         description: 'Mediocre, feel like I could make it myself.',
@@ -62,16 +62,73 @@ const matchaExperiences = [
     },
     {
         location: 'Haus Coffee',
-        dateTime: new Date(2024, 4, 4),
+        dateTime: new Date(2025, 4, 4),
         cardImages: ['assets/images/matcha/haus.jpeg'],
         gmapsLink: 'https://g.co/kgs/91KmYwp',
         description: 'Banana pudding matcha was AMAZING. Banana pudding and matcha go well together, but matcha standalone was already smooth. $9.50 but worth it! Cafe has interesting retro vibe (and interesting music...).',
         grassyLevel: 1,
         tasteLevel: 5
-    }
+    },
+    {
+        location: 'Cafe Wabi Sabi',
+        dateTime: new Date(2025, 5, 24),
+        cardImages: ['assets/images/matcha/cafe-wabi-sabi.HEIC'],
+        gmapsLink: 'https://maps.app.goo.gl/Xdv3CtsyuXFgeXH29',
+        description: 'I THINK it was not bad, just too long ago to remember. Ratings might be abit random.',
+        grassyLevel: 3,
+        tasteLevel: 3
+    },
+    {
+        location: 'Wooly\'s Bagels Arab Street',
+        dateTime: new Date(2025, 6, 20),
+        cardImages: ['assets/images/matcha/wooly-bagels.HEIC'],
+        gmapsLink: 'https://maps.app.goo.gl/xghJRxDsiYoqVUtx8',
+        description: 'Strawberry matcha was kinda sweet but definitely worth a try. Liked the aesthetic of the drink too.',
+        grassyLevel: 1,
+        tasteLevel: 4
+    },
+    {
+        location: 'HEYTEA Vivo',
+        dateTime: new Date(2025, 7, 25),
+        cardImages: ['assets/images/matcha/hey-tea-vivo.HEIC'],
+        gmapsLink: 'https://maps.app.goo.gl/xZi8QngnKuvvSA55A',
+        description: 'Queued 15 mins for this 1 for 1 promo. Got the Supreme Matcha Latte and Triple Supreme Matcha Latte. Tasted pretty good but wish the warabimochi was more silky. Can\'t complain for the price tho.',
+        grassyLevel: 2,
+        tasteLevel: 4
+    },
+    {
+        location: 'Nibbies',
+        dateTime: new Date(2025, 8, 3),
+        cardImages: ['assets/images/matcha/nibbies.HEIC'],
+        gmapsLink: 'https://maps.app.goo.gl/EUgNsAaW4Kk3AbCM9',
+        description: 'This place was supppperrr quiet, but the board game cafe concept was nice. Ordered the Ube matcha upon recommendation. Could taste the Ube but didn\'t feel that it was very well infused with the matcha.',
+        grassyLevel: 2,
+        tasteLevel: 2
+    },
+    {
+        location: 'Hi Coffee',
+        dateTime: new Date(2025, 8, 24),
+        cardImages: ['assets/images/matcha/hi-coffee.HEIC'],
+        gmapsLink: 'https://maps.app.goo.gl/j34wyye9wi1vq5F7A',
+        description: 'Ordered 2 brands of matcha from them - Ajisai and Niko Neko. One cup for $5 from a hawker stall is OK. Matcha is gau, and awesome, especially the pistachio tints from the Niko Neko.',
+        grassyLevel: 1,
+        tasteLevel: 5
+    },
 ]
 
 matchaExperiences.forEach(exp => {exp.dateTime.setMonth(exp.dateTime.getMonth() - 1)});
+
+// TODO add more from google sheets
+
+// matchaExperiences.push( {
+//     location: 'KOKO Cafe and Patisserie',
+//     dateTime: new Date(2024, 3, 26),
+//     cardImages: ['assets/images/matcha/koko.HEIC'],
+//     gmapsLink: 'https://g.co/kgs/C2V61kP',
+//     description: 'Mediocre, feel like I could make it myself.',
+//     grassyLevel: 3,
+//     tasteLevel: 2
+// });
 
 const cards = document.querySelector(".matcha-cards");
 
